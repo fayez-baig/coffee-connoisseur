1605948831ae9a624b6c16adfebd7a9f01054056
fayez-baig <coderninja16@gmail.com>
2022-03-12 16:34:17 +0530

    removed hard coded link

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -26,10 +26,7 @@ const CoffeeStore = ({ data: { name, address, neighbourhood, imgUrl } }) => {
               <h1 className={styles.name}>{name}</h1>
             </div>
             <Image
-              src={
-                imgUrl ||
-                "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-              }
+              src={imgUrl}
               width={600}
               height={360}
               className={styles.storeImg}
